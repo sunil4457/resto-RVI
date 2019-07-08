1fe9d8f8f126f6fc9b8d4e64999fa51502b5fae3
Manjeet <sharma.manjeet@gmail.com>
2019-07-08 11:23:40 +0530

    first commit

---
+++ src/main/java/com/foody/resto/RVI/RestoRviApplication.java
@@ -0,0 +1,13 @@
+package com.foody.resto.RVI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestoRviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestoRviApplication.class, args);
+	}
+
+}
